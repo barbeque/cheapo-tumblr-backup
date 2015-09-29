05d8df3a760b4c4ad2c39e36c18fa1c38f459213
mike <github@ravuya.com>
2015-09-29 10:26:22 -0600

    initial commit

---
+++ scrape.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "https://api.tumblr.com/v2/blog/seat-safety-switch.tumblr.com/posts/text"
+
+# move offset parameter
+# check response.posts.body, response.posts.title for content
+# check response.total_posts for limit on offset parameter
