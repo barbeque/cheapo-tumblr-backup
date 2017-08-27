6957da12780ec9e9b5324b4008f7125c7a255236
Morteza Kalantar <morteza.kalantar@gmail.com>
2017-08-27 19:04:16 +0430

    python3 compatibility (#5)

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ import urllib3
 
 urllib3.disable_warnings() # disable ssl InsecurePlatform warning...
 
-url = "https://api.tumblr.com/v2/blog/seat-safety-switch.tumblr.com/posts/text"
+url = "https://api.tumblr.com/v2/blog/ameliastardust.tumblr.com/posts/text"
 
 # move offset parameter
 # check response.posts.body, response.posts.title for content
@@ -16,7 +16,7 @@ class TumblrEntry:
     def __init__(self, title, body, url):
         self.title = title
         self.body = body
-	self.url = url
+        self.url = url
 
 def get_post_count():
     r = requests.get(url, params = {'api_key': api_key})
@@ -39,7 +39,7 @@ def get_entries(page_number, page_size=20):
 
 def panic_on_bad_status(resp):
     if resp.status_code != 200:
-        print 'Unexpected status code', resp.status_code
+        print('Unexpected status code: {0:d}'.format(resp.status_code))
 
 def get_from_config(config_key):
     import yaml, os.path
@@ -54,15 +54,14 @@ api_key = get_from_config('api_key')
 total_posts = get_post_count()
 page_size = 20 # default of tumblr api, probably a reasonable limit
 pages = int(math.ceil(total_posts/page_size)) + 1
-print 'Expecting to download', pages, 'pages'
+print('Expecting to download {0:d} pages'.format(pages))
 
 all_posts = []
 
 for i in range(0, pages):
     posts_this_page = get_entries(i, page_size)
     all_posts.extend(posts_this_page)
-    print 'scraped page', (i + 1)
-
+    print('scraped page {0:d}'.format(i + 1))
     # don't be a dick, sleep between hits
     time.sleep(1)
 
@@ -71,9 +70,17 @@ with open("posts.html", "w") as f:
     f.write("<head><meta charset='UTF-8'/></head>\n")
     f.write("<body>\n")
     for post in all_posts:
-        title = '<null>' if post.title == None else post.title.encode('utf-8')
-        f.write("<H1>" + title + "</H1>\n")
-        f.write(post.body.encode('utf-8') + "\n")
-	f.write("<a href='" + post.url.encode('utf-8') + "'>#</a>\n");
+        # python2
+        if type(post.url) is not str:
+            title = '<null>' if post.title is None else post.title.encode('utf-8')
+            f.write("<H1>" + title + "</H1>\n")
+            f.write(post.body.encode('utf-8') + "\n".encode('utf-8'))
+            f.write("<a href='" + post.url.encode('utf-8') + "'>#</a>\n")
+        # python3
+        else:
+            title = '<null>' if post.title is None else post.title
+            f.write("<H1>" + title + "</H1>\n")
+            f.write(post.body + "\n")
+            f.write("<a href='" + post.url + "'>#</a>\n")
         f.write("<hr/>\n")
     f.write("</body>\n")
